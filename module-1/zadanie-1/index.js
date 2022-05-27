1fea588063122021f2c5d2b48bbe4ea401ea9ee8
Poocinka15 <poociek15@gmail.com>
2022-05-27 21:21:30 +0200

    Zadanie 1

--- module-1/zadanie-1/index.js
+++ module-1/zadanie-1/index.js
@@ -1,1 +1,19 @@
-console.log("Hello World");
+const year = 1300;
+
+let isLeap;
+
+if (year % 400 == 0) {
+    isLeap = true;
+} else if (year % 100 == 0) {
+    isLeap = false;
+} else if (year % 4 == 0) {
+    isLeap = true;
+} else {
+    isLeap = false;
+}
+
+if (isLeap) {
+    console.log(`${year} is a leap year.`);
+} else {
+    console.log(`${year} is NOT a leap year.`);
+}
