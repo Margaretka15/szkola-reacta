f37abc465d41b5647fc4860a4047be6a5956fd03
Poocinka15 <poociek15@gmail.com>
2022-05-27 22:23:40 +0200

    Refactoring

--- module-1/zadanie-1/index.js
+++ module-1/zadanie-1/index.js
@@ -1,18 +1,19 @@
 const year = 1300;
 
-let isLeap;
+const isLeap = (year) => {
+    if (year % 400 == 0)
+        return true;
 
-if (year % 400 == 0) {
-    isLeap = true;
-} else if (year % 100 == 0) {
-    isLeap = false;
-} else if (year % 4 == 0) {
-    isLeap = true;
-} else {
-    isLeap = false;
+    if (year % 100 == 0)
+        return false;
+
+    if (year % 4 == 0)
+        return true;
+
+    return false;
 }
 
-if (isLeap) {
+if (isLeap(year)) {
     console.log(`${year} is a leap year.`);
 } else {
     console.log(`${year} is NOT a leap year.`);
